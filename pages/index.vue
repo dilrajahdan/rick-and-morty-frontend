0bb197bc1234ba8359e2c2b55dc69c54fae0eaa2
Dil Ahdan <dilrajahdan@gmail.com>
2022-11-29 00:49:39 +0000

    add dialog for details

--- pages/index.vue
+++ pages/index.vue
@@ -4,35 +4,76 @@
       <h1 class="text-h2 text-center">Rick and Morty</h1>
     </v-sheet>
 
-    <v-container>
+    <v-container fluid class="px-16">
       <v-row justify="center" align="center">
         <v-col
           v-for="character in characters"
           :key="character.id"
           cols="12"
-          sm="8"
-          md="3"
+          sm="6"
+          md="4"
+          lg="3"
+          xl="2"
         >
-          <v-card class="mx-auto" :to="`/character/${character.id}`">
-            <!-- <v-parallax :src="character.image" height="200px"> -->
-            <!-- <v-card-title class="headline white--text">{{
-            character.name
-          }}</v-card-title> -->
-            <v-img :src="character.image" height="200px"></v-img>
+          <v-card class="mx-auto" @click="getCharacter(character.id)">
+            <v-img :src="character.image"></v-img>
             <v-card-title class="headline">{{ character.name }}</v-card-title>
             <v-card-text>
               {{ character.species }}
             </v-card-text>
-            <!-- </v-parallax> -->
           </v-card>
         </v-col>
+      </v-row>
+      <v-row>
+        <v-col class="text-center">
+          <!-- {{ paging }} -->
 
-        <!-- {{ paging }} -->
-
-        <v-btn @click="prevPage()">Prev</v-btn>
-        <v-btn @click="nextPage()">Next</v-btn>
+          <v-btn
+            x-large
+            color="pink"
+            :disabled="!paging.prev"
+            @click="prevPage()"
+            >Prev</v-btn
+          >
+          <v-btn
+            x-large
+            color="pink"
+            :disabled="!paging.next"
+            class="ml-6"
+            @click="nextPage()"
+            >Next</v-btn
+          >
+        </v-col>
       </v-row>
     </v-container>
+
+    <!-- Dialog with character details -->
+    <v-dialog v-model="dialog" max-width="900px">
+      <v-card>
+        <v-card-title class="headline">{{ character.name }}</v-card-title>
+
+        <v-card-text>
+          <v-row>
+            <v-col cols="12" sm="6" md="4">
+              <v-img :src="character.image" height="200px"></v-img>
+            </v-col>
+            <v-col cols="12" sm="6" md="8">
+              <!-- get name, status, species, gender, origin,
+last known location, image, number of episodes appearances -->
+
+              <p><strong>Name:</strong> {{ character.name }}</p>
+              <p><strong>Species:</strong> {{ character.species }}</p>
+              <p><strong>Status:</strong> {{ character.status }}</p>
+              <p><strong>Gender:</strong> {{ character.gender }}</p>
+              <p><strong>Origin:</strong> {{ character.origin.name }}</p>
+              <p><strong>Location:</strong> {{ character.location.name }}</p>
+              <p><strong>Img:</strong> {{ character.image }}</p>
+              <p><strong>Episodes:</strong> {{ character.episode }}</p>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </article>
 </template>
 
@@ -48,8 +89,10 @@ export default {
     return { characters: data.results, paging: data.info }
   },
   data: () => ({
+    character: [],
     characters: [],
     paging: {},
+    dialog: false,
   }),
   // setup paging
   methods: {
@@ -62,6 +105,14 @@ export default {
       const { data } = await this.$axios.get(this.paging.prev)
       this.characters = data.results
       this.paging = data.info
+    },
+    // get character details
+    async getCharacter(id) {
+      const { data } = await this.$axios.get(
+        `https://rickandmortyapi.com/api/character/${id}`
+      )
+      this.character = data
+      this.dialog = true
     },
   },
 }
